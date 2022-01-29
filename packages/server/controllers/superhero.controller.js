9e125fcc54586031090e9578d74020afc6799457
julietvt <freshitpt@gmail.com>
2022-01-29 13:37:39 +0200

    superhero controller. method getMane

--- packages/server/controllers/superhero.controller.js
+++ packages/server/controllers/superhero.controller.js
@@ -14,7 +14,36 @@ export async function create(req, res, next) {
   }
 }
 
-export async function getMany(req, res, next) {}
+//http://127.0.0.1:5000/api/superheroes?page=1&items=5
+
+/*
+client
+
+const apiInstance = axios.create({
+  baseURL: 'http://127.0.0.1:5000/api'
+});
+const getSuperheroes = (params) => apiInstance.get(`/superheroes?page=${params.page}&results=${params.results}`); 
+---------------
+const {data { data: superheroes }} = API.getSuperheroes(params);
+
+
+*/
+
+export async function getMany(req, res, next) {
+  const {
+    pagination: { page, results },
+  } = req;
+  try {
+    const foundHeroes = await Superhero.findAll({
+      limit: results,
+      offset: (page - 1) * results,
+    });
+    res.status(200).send({ data: foundHeroes });
+  } catch (err) {
+    next(err);
+  }
+}
+
 export async function getById(req, res, next) {}
 export async function updateInfoById(req, res, next) {}
 export async function updateImageById(req, res, next) {}
